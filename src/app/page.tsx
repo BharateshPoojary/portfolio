805fecbf007c83b71cc49a3bf100b217c2759828
Bharatesh Poojary <bharateshpoojari@gmail.com>
2025-01-13 23:10:29 +0530

    framer motion usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
       >
         <div className="grid grid-cols-2">
           <h3 className="text-4xl font-bold">
-            <span className="text-6xl">Bharatesh here,</span>a Full Stack
-            Developer passionate about developing web applicatons using MERN
-            stack.
+            <span className="text-6xl">Hi, I am Bharatesh Poojary </span>a Full
+            Stack Developer passionate about developing web applicatons using
+            MERN stack.
           </h3>
         </div>
       </div>
